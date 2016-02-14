fbd720e07be3a4dcef844aad5dce5e33def30413
Jim Downhower <jdownhower@gmail.com>
2016-02-14 11:10:49 -0500

    completed 3.3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ var BugList = React.createClass({
                 <h1>Bug Tracker</h1>
                 <BugFilter />
                 <hr />
-                <BugTable />
+                <BugTable>
+                    <BugRow id="1" status="Open" priority="High" owner="Fred" title="Page Fault" />
+                    <BugRow id="2" status="Test" priority="Low" owner="Barney" title="General Protection Fault" />
+                </BugTable>
                 <hr />
                 <BugAdd />
             </div>
@@ -26,9 +29,35 @@ var BugFilter = React.createClass({
 var BugTable = React.createClass({
     render: function() {
         return(
-            <div>
-                Hello, I am a BugTable....list of all bugs
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Status</th>
+                        <th>Priority</th>
+                        <th>Owner</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.children}
+                </tbody>
+            </table>
+        );
+    }
+});
+
+// Single table row for a bug
+var BugRow = React.createClass({
+    render: function() {
+        return(
+            <tr>
+                <td>{this.props.id}</td>
+                <td>{this.props.status}</td>
+                <td>{this.props.priority}</td>
+                <td>{this.props.owner}</td>
+                <td>{this.props.title}</td>
+            </tr>
         );
     }
 });
--- static/App.js
+++ static/App.js
@@ -1,56 +1,135 @@
 var BugList = React.createClass({
-    displayName: 'BugList',
+    displayName: "BugList",
 
     render: function () {
         return React.createElement(
-            'div',
+            "div",
             null,
             React.createElement(
-                'h1',
+                "h1",
                 null,
-                'Bug Tracker'
+                "Bug Tracker"
             ),
             React.createElement(BugFilter, null),
-            React.createElement('hr', null),
-            React.createElement(BugTable, null),
-            React.createElement('hr', null),
+            React.createElement("hr", null),
+            React.createElement(
+                BugTable,
+                null,
+                React.createElement(BugRow, { id: "1", status: "Open", priority: "High", owner: "Fred", title: "Page Fault" }),
+                React.createElement(BugRow, { id: "2", status: "Test", priority: "Low", owner: "Barney", title: "General Protection Fault" })
+            ),
+            React.createElement("hr", null),
             React.createElement(BugAdd, null)
         );
     }
 });
 
 var BugFilter = React.createClass({
-    displayName: 'BugFilter',
+    displayName: "BugFilter",
 
     render: function () {
         return React.createElement(
-            'div',
+            "div",
             null,
-            'Hello, I am a BugFilter...a filter'
+            "Hello, I am a BugFilter...a filter"
         );
     }
 });
 
 var BugTable = React.createClass({
-    displayName: 'BugTable',
+    displayName: "BugTable",
 
     render: function () {
         return React.createElement(
-            'div',
+            "table",
             null,
-            'Hello, I am a BugTable....list of all bugs'
+            React.createElement(
+                "thead",
+                null,
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        null,
+                        "ID"
+                    ),
+                    React.createElement(
+                        "th",
+                        null,
+                        "Status"
+                    ),
+                    React.createElement(
+                        "th",
+                        null,
+                        "Priority"
+                    ),
+                    React.createElement(
+                        "th",
+                        null,
+                        "Owner"
+                    ),
+                    React.createElement(
+                        "th",
+                        null,
+                        "Title"
+                    )
+                )
+            ),
+            React.createElement(
+                "tbody",
+                null,
+                this.props.children
+            )
+        );
+    }
+});
+
+// Single table row for a bug
+var BugRow = React.createClass({
+    displayName: "BugRow",
+
+    render: function () {
+        return React.createElement(
+            "tr",
+            null,
+            React.createElement(
+                "td",
+                null,
+                this.props.id
+            ),
+            React.createElement(
+                "td",
+                null,
+                this.props.status
+            ),
+            React.createElement(
+                "td",
+                null,
+                this.props.priority
+            ),
+            React.createElement(
+                "td",
+                null,
+                this.props.owner
+            ),
+            React.createElement(
+                "td",
+                null,
+                this.props.title
+            )
         );
     }
 });
 
 var BugAdd = React.createClass({
-    displayName: 'BugAdd',
+    displayName: "BugAdd",
 
     render: function () {
         return React.createElement(
-            'div',
+            "div",
             null,
-            'Hello, I am a BugAdd...a form to add a new bug'
+            "Hello, I am a BugAdd...a form to add a new bug"
         );
     }
 });
