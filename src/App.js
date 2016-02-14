d4cd9e52eb7dab53a01a3cf672a41814f4b10174
Jim Downhower <jdownhower@gmail.com>
2016-02-13 23:18:35 -0500

    completed 3.2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,51 @@
+var BugList = React.createClass({
+    render: function() {
+        return(
+            <div>
+                <h1>Bug Tracker</h1>
+                <BugFilter />
+                <hr />
+                <BugTable />
+                <hr />
+                <BugAdd />
+            </div>
+        );
+    }
+});
+
+var BugFilter = React.createClass({
+    render: function() {
+        return(
+            <div>
+                Hello, I am a BugFilter...a filter
+            </div>
+        );
+    }
+});
+
+var BugTable = React.createClass({
+    render: function() {
+        return(
+            <div>
+                Hello, I am a BugTable....list of all bugs
+            </div>
+        );
+    }
+});
+
+var BugAdd = React.createClass({
+    render: function() {
+        return(
+            <div>
+                Hello, I am a BugAdd...a form to add a new bug
+            </div>
+        );
+    }
+});
+
+
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('example')
+    <BugList />,
+    document.getElementById('main')
 );
--- static/App.js
+++ static/App.js
@@ -1,5 +1,58 @@
-ReactDOM.render(React.createElement(
-    'h1',
-    null,
-    'Hello, world!'
-), document.getElementById('example'));
+var BugList = React.createClass({
+    displayName: 'BugList',
+
+    render: function () {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h1',
+                null,
+                'Bug Tracker'
+            ),
+            React.createElement(BugFilter, null),
+            React.createElement('hr', null),
+            React.createElement(BugTable, null),
+            React.createElement('hr', null),
+            React.createElement(BugAdd, null)
+        );
+    }
+});
+
+var BugFilter = React.createClass({
+    displayName: 'BugFilter',
+
+    render: function () {
+        return React.createElement(
+            'div',
+            null,
+            'Hello, I am a BugFilter...a filter'
+        );
+    }
+});
+
+var BugTable = React.createClass({
+    displayName: 'BugTable',
+
+    render: function () {
+        return React.createElement(
+            'div',
+            null,
+            'Hello, I am a BugTable....list of all bugs'
+        );
+    }
+});
+
+var BugAdd = React.createClass({
+    displayName: 'BugAdd',
+
+    render: function () {
+        return React.createElement(
+            'div',
+            null,
+            'Hello, I am a BugAdd...a form to add a new bug'
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(BugList, null), document.getElementById('main'));
