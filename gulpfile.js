30c125f6a91dccedee15f051649bc5517b3f747c
Jim Downhower <jdownhower@gmail.com>
2016-02-17 10:34:05 -0500

    completed 7.3

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+
 
 gulp.task('bundle', function(){
     return browserify('src/App.js')
@@ -10,4 +12,30 @@ gulp.task('bundle', function(){
         .pipe(source('bundle.js'))
         // Start piping stream to tasks!
         .pipe(gulp.dest("static/"));
+});
+
+
+gulp.task('watch', function(){
+    var b = browserify({
+        entries: ['src/App.js'],
+        cache: {},
+        packageCache: {},
+        pugin: ['watchify']
+    });
+    
+    function makeBundle() {
+        b.transform('babelify', {presets: ['react']})
+            .bundle()
+            //Pass desired output filename to vinyl-source-stream
+            .pipe(source('bundle.js'))
+            // Start piping stream to tasks!
+            .pipe(gulp.dest("static/"));
+    }
+    
+    b.on('update', makeBundle);
+
+    //we have to call bundle once to kick it off
+    makeBundle();
+        
+    return b;
 });
