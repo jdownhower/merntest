6ed095de46c7158993f1315b4fb96ad9ace2fe9c
Jim Downhower <jdownhower@gmail.com>
2016-02-20 10:19:47 -0500

    completed 9.1 - but had to use old router version and not working perfectly

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Link = require('react-router').Link;
+var hashHistory = require('react-router').hashHistory;
+
 var BugList = require('./BugList.jsx');
 
+var NoMatch = React.createClass({
+    render: function() {
+        return(
+            <h2>No match for the route</h2>
+        );
+    }
+});
+
+
 ReactDOM.render(
-    <BugList />,
+    <Router history={hashHistory}>
+        <Route path="/bugs" component={BugList}/>
+        <Redirect from="/" to="/bugs" /> 
+        <Route path="*" component={NoMatch} />
+    </Router>,
     document.getElementById('main')
 );
